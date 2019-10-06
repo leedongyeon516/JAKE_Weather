ede40adaebce2fe643571a716a8a936a574d7a75
JAKE LEE <lee@JAKEs-MacBook-Pro.local>
2019-10-06 04:58:14 -0400

    Updates

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ const searchSection = document.querySelector(".search-section h1");
 const searchBtn = document.querySelector(".search-btn");
 const resultsSection = document.querySelector(".results-section");
 const information = document.querySelector(".information");
+const informationContent = document.querySelector(".information-content");
 const modal = document.querySelector(".modal-modified");
 const modalContent = document.querySelector(".modal-content-modified p");
 const closeBtn = document.querySelector(".close-btn");
@@ -57,8 +58,11 @@ function geocode(e) {
               temperature,
               humidity,
               windSpeed,
+              visibility,
               uvIndex
             } = data.currently;
+            const { precipProbability } = data.daily.data[0];
+            const { temperatureLow, temperatureHigh } = data.daily.data[1];
 
             let weatherNight = `
                   <div class="stars"></div>
@@ -123,7 +127,50 @@ function geocode(e) {
                 "linear-gradient(rgb(4, 4, 7), rgb(37, 103, 170))";
             }
 
+            let weatherInfo = `
+                  <div class="data">
+                    <div class="today">
+                      <p><span class="color">Today</span><br>${
+                        data.daily.data[0].summary
+                      }</p>
+                      <P>Temperature <span class="color">${
+                        data.daily.data[0].temperatureLow
+                      } F - ${data.daily.data[0].temperatureHigh} F</span></p>
+                      <P>Chance of rain <span class="color">${Math.floor(
+                        precipProbability * 100
+                      )} %</span></P>
+                      <P>Humidity <span class="color">${humidity *
+                        100} %</span></P>
+                      <P>Wind <span class="color">${windSpeed} km/h</span></P>
+                      <P>Visibility <span class="color">${visibility} km</span></P>
+                      <P>UV <span class="color">${uvIndex}</span></P>
+                    </div>
+                    <div class="tomorrow">
+                      <p><span class="color">Tomorrow</span><br>${
+                        data.daily.data[1].summary
+                      }</p>
+                      <P>Temperature <span class="color">${temperatureLow} F - ${temperatureHigh} F</span></p>
+                      <P>Chance of rain <span class="color">${Math.floor(
+                        data.daily.data[1].precipProbability * 100
+                      )} %</span></P>
+                      <P>Humidity <span class="color">${data.daily.data[1]
+                        .humidity * 100} %</span></P>
+                      <P>Wind <span class="color">${
+                        data.daily.data[1].windSpeed
+                      } km/h</span></P>
+                      <P>Visibility <span class="color">${
+                        data.daily.data[1].visibility
+                      } km</span></P>
+                      <P>UV <span class="color">${
+                        data.daily.data[1].uvIndex
+                      }</span></P>
+                    </div>
+                  </div>`;
+
             document.querySelector(".results-window").innerHTML = output;
+            document.querySelector(
+              ".information-content"
+            ).innerHTML = weatherInfo;
 
             searchBtn.innerHTML = "I N F O";
           });
@@ -137,14 +184,21 @@ function geocode(e) {
   }
 }
 
-closeBtn.addEventListener("click", function() {
+closeBtn.addEventListener("click", () => {
   modal.style.display = "none";
 });
 
-window.addEventListener("click", function(e) {
-  if (e.target == modal || e.target == information) {
-    modal.style.display = "none";
+informationContent.addEventListener("click", () => {
+  if (information.style.display === "none") {
+    information.style.display = "block";
+  } else {
     information.style.display = "none";
+  }
+});
+
+window.addEventListener("click", e => {
+  if (e.target == modal) {
+    modal.style.display = "none";
   }
 });
 
